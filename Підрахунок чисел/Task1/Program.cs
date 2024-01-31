b1895d378ccd05be6aec9ff73df1b390c48cd15f
YanaShuliak <yanashuliaky@gmail.com>
2024-01-31 17:18:36 +0200

    Створенний файл .gitignore та README, а також написаний код для обрахунку суми чесел

---
+++ Підрахунок чисел/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            double first, second, suma;
+            Console.Write("Введіть перше значення: ");
+            while (!double.TryParse(Console.ReadLine(), out first))
+            {
+                Console.WriteLine("Ви ввели не правильне значення!");
+                Console.Write("Введіть перше значення: ");
+            }
+
+            Console.Write("Введіть друге значення: ");
+            while (!double.TryParse(Console.ReadLine(), out second))
+            {
+                Console.WriteLine("Ви ввели не правильне значення!");
+                Console.Write("Введіть друге значення: ");
+            }
+            suma = first + second;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Сума чисел: {suma}");
+            Console.ReadKey();  
+
+        }
+    }
+}
